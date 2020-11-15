30261ea257a34dae3feb9ee4ab2361bf55d6bb67
Svetlanchikus <70448330+Svetlanchikus@users.noreply.github.com>
2020-11-15 16:46:38 +0300

    Update Сalculator.java
    Исправление ДЗ5

--- src/Lesson_5/Logger/Сalculator.java
+++ src/Lesson_5/Logger/Сalculator.java
@@ -13,100 +13,35 @@ public class Сalculator {
         return firstVariable;
     }
 
-    public void setFirstVariable(double firstVariable) {
-        this.firstVariable = firstVariable;
-    }
-
     public double getSecondVariable() {
         return secondVariable;
     }
 
-    public void setSecondVariable(double secondVariable) {
-        this.secondVariable = secondVariable;
+    LoggerFactory factory = new FileLoggerFactory();
+    Logger logger = factory.createLogger();
+
+    public double addition() {
+        double result = getFirstVariable() + getSecondVariable();
+        logger.log("Результат сложения: " + result);
+        return result;
     }
 
-    public void addition() {
-        LoggerFactory factory = null;
-
-        if (getFirstVariable() == 0 && getSecondVariable() == 0) {
-            factory = new ConsoleLoggerFactore();
-            Logger logger = factory.createLogger();
-            logger.log("Суммирование не производится. Значение равно 0");
-        } else if (getFirstVariable() == 0 && getSecondVariable() != 0) {
-            factory = new FileLoggerFactory();
-            Logger logger = factory.createLogger();
-            logger.log("Запись в файл второго значения " + getSecondVariable());
-        } else if (getFirstVariable() != 0 && getSecondVariable() == 0) {
-            factory = new FileLoggerFactory();
-            Logger logger = factory.createLogger();
-            logger.log("Запись в файл первого значения " + getFirstVariable());
-        } else {
-            factory = new DbLoggerFactory();
-            Logger logger = factory.createLogger();
-            logger.log("Производится запись в БД суммы первого и второго аргумента равна: " + (getFirstVariable() + getSecondVariable()));
-        }
+    public double subtraction() {
+        double result = getFirstVariable() - getSecondVariable();
+        logger.log("Результат вычитания: " + result);
+        return result;
     }
 
-    public void subtraction() {
-        LoggerFactory factory = null;
-
-        if (getFirstVariable() == 0 && getSecondVariable() == 0) {
-            factory = new ConsoleLoggerFactore();
-            Logger logger = factory.createLogger();
-            logger.log("Вычитание не производится. Значение равно 0");
-        } else if (getFirstVariable() == 0 && getSecondVariable() != 0) {
-            factory = new FileLoggerFactory();
-            Logger logger = factory.createLogger();
-            logger.log("Запись в файл отрицательного второго значения " + getSecondVariable());
-        } else if (getFirstVariable() != 0 && getSecondVariable() == 0) {
-            factory = new FileLoggerFactory();
-            Logger logger = factory.createLogger();
-            logger.log("Запись в файл первого значения " + getFirstVariable());
-        } else if (getFirstVariable() < getSecondVariable()) {
-            factory = new ConsoleLoggerFactore();
-            Logger logger = factory.createLogger();
-            logger.log("При вычислении получается отрицательное значение: " + (getFirstVariable() - getSecondVariable()));
-        } else {
-            factory = new DbLoggerFactory();
-            Logger logger = factory.createLogger();
-            logger.log("Производится запись в БД вычитания из первого аргумента второго: " + (getFirstVariable() * getSecondVariable()));
-        }
+    public double multiplication() {
+        double result = getFirstVariable() * getSecondVariable();
+        logger.log("Результат умножения: " + result);
+        return result;
     }
 
-    public void multiplication() {
-        LoggerFactory factory = null;
-
-        if (getFirstVariable() == 0 || getSecondVariable() == 0) {
-            factory = new ConsoleLoggerFactore();
-            Logger logger = factory.createLogger();
-            logger.log("Умножение не производится. Значение равно 0");
-        } else {
-            factory = new DbLoggerFactory();
-            Logger logger = factory.createLogger();
-            logger.log("Производится запись в БД произведения первого и второго аргумента равна: " + (getFirstVariable() * getSecondVariable()));
-        }
-    }
-
-    public void division() {
-        LoggerFactory factory = null;
-
-        if (getFirstVariable() == 0 && getSecondVariable() == 0) {
-            factory = new ConsoleLoggerFactore();
-            Logger logger = factory.createLogger();
-            logger.log("Деление не производится. Значение равно 0");
-        } else if (getFirstVariable() == 0 && getSecondVariable() != 0) {
-            factory = new ConsoleLoggerFactore();
-            Logger logger = factory.createLogger();
-            logger.log("Деление не производится. Значение равно 0");
-        } else if (getFirstVariable() != 0 && getSecondVariable() == 0) {
-            factory = new ConsoleLoggerFactore();
-            Logger logger = factory.createLogger();
-            logger.log("Ошибка. Деление на ноль недопустимо");
-        } else {
-            factory = new DbLoggerFactory();
-            Logger logger = factory.createLogger();
-            logger.log("Производится запись в БД значения деления первого и второго аргумента: " + (getFirstVariable() / getSecondVariable()));
-        }
+    public double division() {
+        double result = getFirstVariable() / getSecondVariable();
+        logger.log("Результат деления: " + result);
+        return result;
     }
 
 }
